053c9a52c612b9acbc4c0e7f3eca192fbe3f784e
Marian Steinbach <marian@sendung.de>
2012-12-17 22:24:04 +0100

    Angepasstes Template fuer Latex und PDF

--- scripts/create_documents.sh
+++ scripts/create_documents.sh
@@ -20,10 +20,11 @@ pandoc -t html5 -s -N -o $DOC_FOLDER/html5/document.html $SOURCE
 cp -r images $DOC_FOLDER/html5/images
 
 # TeX
-pandoc -t latex -o $DOC_FOLDER/latex/document.tex $SOURCE
+pandoc -t latex --template $DOC_FOLDER/latex/template.tex \
+	-o $DOC_FOLDER/latex/document.tex $SOURCE
 
 # OpenOffice odt
-pandoc -t odt -o $DOC_FOLDER/odt/document.odt $SOURCE
+pandoc --table-of-contents -t odt -o $DOC_FOLDER/odt/document.odt $SOURCE
 
 # docbook
 pandoc -t docbook -o $DOC_FOLDER/docbook/document.xml $SOURCE
@@ -32,13 +33,14 @@ pandoc -t docbook -o $DOC_FOLDER/docbook/document.xml $SOURCE
 pandoc -t epub -o $DOC_FOLDER/epub/document.epub $SOURCE
 
 # Word .docx
-pandoc -t docx -o $DOC_FOLDER/docx/document.docx $SOURCE
+pandoc --table-of-contents -t docx -o $DOC_FOLDER/docx/document.docx $SOURCE
 
 # plain text
-pandoc -t plain -o $DOC_FOLDER/plain/document.txt $SOURCE
+pandoc --table-of-contents -t plain -o $DOC_FOLDER/plain/document.txt $SOURCE
 
 # PDF
-pandoc --latex-engine=$PDFLATEX -N -o $DOC_FOLDER/pdf/document.pdf $SOURCE
+pandoc --latex-engine=$PDFLATEX --table-of-contents --template $DOC_FOLDER/latex/template.tex -N \
+	-o $DOC_FOLDER/pdf/document.pdf $SOURCE
 
 
 cd $PWD

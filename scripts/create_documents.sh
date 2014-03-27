2c7065a118bbe9ee1c9f7b6084bc99ed66536bfc
Marian Steinbach <marian@sendung.de>
2014-03-27 16:57:07 +0100

    Derivat-Erzeugung geändert
    - header_attributes aktiviert.
    Erfordert das aktuelle pandoc (1.12)

--- scripts/create_documents.sh
+++ scripts/create_documents.sh
@@ -21,46 +21,46 @@ do
 		-sOutputFile=${f%.*}.png -f $f
 done
 
+# PDF
+echo "Generiere PDF-Dokument"
+pandoc --latex-engine=$PDFLATEX --table-of-contents --template $DOC_FOLDER/latex/template.tex -N \
+	-o $DOC_FOLDER/pdf/document.pdf -f markdown+header_attributes $SOURCE
 
 # HTML
 echo "Generiere HTML-Dokument"
-pandoc -t html -s -N -o $DOC_FOLDER/html/document.html $SOURCE
+pandoc -t html -s -N -o $DOC_FOLDER/html/document.html -f markdown+header_attributes $SOURCE
 cp -r images $DOC_FOLDER/html/images
 
 # HTML5
 echo "Generiere HTML5-Dokument"
-pandoc -t html5 -s -N --section-divs -o $DOC_FOLDER/html5/document.html $SOURCE
+pandoc -t html5 -s -N --section-divs -o $DOC_FOLDER/html5/document.html -f markdown+header_attributes $SOURCE
 cp -r images $DOC_FOLDER/html5/images
 
 # TeX
 echo "Generiere TeX-Dokument"
 pandoc -t latex --template $DOC_FOLDER/latex/template.tex \
-	-o $DOC_FOLDER/latex/document.tex $SOURCE
+	-o $DOC_FOLDER/latex/document.tex -f markdown+header_attributes $SOURCE
 
 # OpenOffice odt
 echo "Generiere OpenOffice/LibreOffice ODT-Dokument"
-pandoc --table-of-contents -t odt -o $DOC_FOLDER/odt/document.odt $SOURCE
+pandoc --table-of-contents -t odt -o $DOC_FOLDER/odt/document.odt -f markdown+header_attributes $SOURCE
 
 # docbook
 echo "Generiere DocBook-Dokument"
-pandoc -t docbook -o $DOC_FOLDER/docbook/document.xml $SOURCE
+pandoc -t docbook -o $DOC_FOLDER/docbook/document.xml -f markdown+header_attributes $SOURCE
 
 # epub
 echo "Generiere EPub-Dokument"
-pandoc -t epub -o $DOC_FOLDER/epub/document.epub $SOURCE
+pandoc -t epub -o $DOC_FOLDER/epub/document.epub -f markdown+header_attributes $SOURCE
 
 # Word .docx
 echo "Generiere Microsoft Office DOCX-Dokument"
-pandoc --table-of-contents -t docx -o $DOC_FOLDER/docx/document.docx $SOURCE
+pandoc --table-of-contents -t docx -o $DOC_FOLDER/docx/document.docx -f markdown+header_attributes $SOURCE
 
 # plain text
 echo "Generiere Plain-Text-Dokument"
-pandoc --table-of-contents -t plain -o $DOC_FOLDER/plain/document.txt $SOURCE
+pandoc --table-of-contents -t plain -o $DOC_FOLDER/plain/document.txt -f markdown+header_attributes $SOURCE
 
-# PDF
-echo "Generiere PDF-Dokument"
-pandoc --latex-engine=$PDFLATEX --table-of-contents --template $DOC_FOLDER/latex/template.tex -N \
-	-o $DOC_FOLDER/pdf/document.pdf $SOURCE
 
 
 cd $PWD

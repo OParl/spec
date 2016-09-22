296b8095046daf0a793fd11d3c5075ff66b5cf9f
konstin <konstin@mailbox.org>
2016-09-22 23:49:23 +0200

    Script für die Beispielüberprüfung verbessern

--- scripts/validate-examples.py
+++ scripts/validate-examples.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 """
-Checks for that the right atttribute types are used in the examples.
+Checks for that the right atttribute types are used in the examples of the OParl
+spec.
 
 To use this as as library, call validate_object() with the jsob loaded into an
-OrderedDict.
+OrderedDict. Requires the schema files of at least version ac8c3b to be in a
+`schema/` folder.
+
+Type Mapping:
+----------------------------
+| OParl JSON | Python      |
+----------------------------
+| object     | OrderedDict |
+| array      | list        |
+| string     | str         |
+| boolean    | bool        |
+| int        | int         |
+----------------------------
 """
 
 import json
@@ -13,14 +26,20 @@ import sys
 
 from collections import OrderedDict
 
-def validate_single_attribut(attribute, value, properties):
+def validate_single_attribut(attribute, value, properties, properties_key):
     """
     Validates one attribute value pair using the schema information given in
     properties. Recursively validates arrays or calls validate_object format
     embedded objects.
 
-    Returns True on succes and a String with the error message in case of a failure
+    Returns True on succes, False on failure without addional error message
+    or a string with an error message
     """
+    if not properties_key in properties.keys():
+        return "The attribute '" + properties_key + "' is not defined in the schema"
+
+    properties = properties[properties_key]
+
     property_type = properties["type"]
     if property_type == "boolean":
         if not type(value) == bool:
@@ -44,11 +63,11 @@ def validate_single_attribut(attribute, value, properties):
         elif subtype == "date":
             date = re.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
             if not date.match(value):
-                return "'" + value + "' is not a valid date as of ISO 8601"
+                return "'" + value + "' is not a valid date"
         elif subtype == "date-time":
             datetime = re.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}[\+\-][0-9]{2}:[0-9]{2}$")
             if not datetime.match(value):
-                return "'" + value + "' is not a valid datetime as of ISO 8601"
+                return "'" + value + "' is not a valid datetime"
         else:
             return "Invalid string type"
     elif property_type == "array":
@@ -57,21 +76,23 @@ def validate_single_attribut(attribute, value, properties):
 
         # Check every element of this list by recursive function calls with some debugging information attached
         for i, j in enumerate(value):
-            if validate_single_attribut(attribute + "[" + str(i) + "]", j, properties["items"]) != True:
-                return "The list '" + attribute + "' has an invalid embedded object"
+            result = validate_single_attribut(attribute + "[" + str(i) + "]", j, properties, "items")
+            if result == False:
+                return False
+            elif type(result) == str:
+                return result
     elif property_type == "object":
         if not type(value) == OrderedDict:
             return "'" + attribute + "' has the type '" + type(value).__name__ + "' instead of the expected type 'OrderedDict'"
 
-        # Got an object, so let's validate it
-        if not validate_object(value):
-            return "The embedded object '" + attribute + "' is invalid due to prior errors"
+        if not validate_object(value, attribute):
+            return False
     else:
         return "Invalid json type: " + property_type
 
     return True
 
-def validate_object(target):
+def validate_object(target, embedded_object = ""):
     """
     Validates a whole object using validate_single_attribut and prints every error
 
@@ -79,44 +100,51 @@ def validate_object(target):
     """
     valid = True
 
-    objects = ["System", "Body", "LegislativeTerm", "Organization", "Person", "Membership", "Meeting", "AgendaItem", "Paper", "Consultation", "File", "Location"]
+    objects = [i for i in os.listdir("schema")]
 
     oparl_type = re.compile(r"^https://schema.oparl.org/1.0/([a-zA-Z]+)$").match(target["type"]).group(1)
     for i in objects:
-        if i.lower() == oparl_type:
-            oparl_type = i
-    schema = json.load(open("schema/" + oparl_type + ".json"), object_pairs_hook=OrderedDict)
+        if i.lower() == oparl_type + ".json":
+            schema_file = i
+    schema = json.load(open(os.path.join("schema/", schema_file)), object_pairs_hook=OrderedDict)
 
     for i in schema["required"]:
         if not i in target.keys() or target[i] == None:
-            return "Required key " + i + " missing."
+            print(" - [ ] Required key " + i + " missing.")
 
-    for (key, value) in target.items():
-        if not key in schema["properties"].keys():
-            print("The attribute '" + key + "' is undefined")
-            valid = False
+    for attribute, value in target.items():
+        result = validate_single_attribut(attribute, value, schema["properties"], attribute)
+        if result == True:
             continue
 
-        result = validate_single_attribut(key, value, schema["properties"][key])
-        if result != True:
-            print(result)
-            valid = False
+        valid = False
+        if type(result) == str:
+            if embedded_object == "":
+                print(" - [ ] " + result)
+            else:
+                print(" - [ ] In the embedded object '" + embedded_object + "': " + result)
+
 
     return valid
 
 def main():
     all_valid = True
+    print("Validating " + str(len(os.listdir("examples"))) + " files ...")
     for file in os.listdir("examples"):
-        print(" --- " + file + " --- ")
+        print("\n#### " + os.path.join("examples", file))
         target = json.load(open(os.path.join("examples", file)), object_pairs_hook=OrderedDict)
         valid = validate_object(target)
-        if valid:
-            print("Passed")
-        else:
-            print("Failed")
+        if valid != True:
             all_valid = False
 
-    if not all_valid:
+    if all_valid:
+        print()
+        print(" --- Passed --- ")
+        print()
+    else:
+        print()
+        print(" --- Failed --- ")
+        print()
         exit(1)
 
 if __name__ == "__main__":

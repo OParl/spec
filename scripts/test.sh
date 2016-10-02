53777c6a2e9cfdd32aaadcf183ccb84c2902ae1e
konstin <konstin@mailbox.org>
2016-10-02 16:19:38 +0200

    jsonlint (und composer) entfernen

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,37 +3,14 @@
 set -e
 
 root=$(git rev-parse --show-toplevel)
-export PATH=${PATH}:${root}/vendor/bin/:~/.composer/vendor/bin/
 cd ${root}
-retval=0
-
-# json-lint all the json
-for f in schema/*.json examples/*.json
-do
-  res=$(jsonlint $f)
-  if [ "$res" != "Valid JSON" ]
-  then
-    retval=1
-  fi
-done
-
-# validate markdown
-cd src
-res=$(pandoc -t json *.md | jsonlint)
-if [ "$res" != "Valid JSON" ]
-then
-  retval=1
-fi
-cd ..
 
 # validate schema and examples
 res=$(scripts/validate-examples.py)
 if [ $? -gt 0 ]
 then
-  retval=1
+  exit 1
 fi
 
 # validate tex-template
 # TODO: validate tex with lacheck or chktex
-
-exit $retval
